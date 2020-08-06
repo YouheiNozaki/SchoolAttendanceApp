ae7dc43bdffb86e6450fb8cb55dd56a34ed96dca
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-08-06 17:06:26 +0900

    Headerコンポーネントを作成

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,66 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import { Home, Help, Settings } from '@material-ui/icons'
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            backgroundColor: theme.palette.primary.main,
+        },
+        title: {
+            paddingLeft: '24px',
+            '& h1': {
+                color: theme.palette.primary.contrastText,
+            },
+        },
+        nav: {
+            '& a': {
+                color: theme.palette.primary.contrastText,
+            },
+            '& a:hover': {
+                color: theme.palette.secondary.light,
+            },
+        },
+    })
+)
 
 export const Header: React.FC = () => {
+    const classes = useStyles()
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/setting">Settings</Link>
-                </li>
-                <li>
-                    <Link to="/question">Question</Link>
-                </li>
-            </ul>
-        </nav>
+        <Grid
+            className={classes.container}
+            container
+            direction="row"
+            alignItems="center"
+        >
+            <Grid className={classes.title} item xs={6}>
+                <h1>残業調整くん</h1>
+            </Grid>
+            <Grid
+                container
+                className={classes.nav}
+                xs={6}
+                spacing={6}
+                justify="flex-end"
+            >
+                <Grid item>
+                    <Link to="/">
+                        <Home fontSize="large" />
+                    </Link>
+                </Grid>
+                <Grid item>
+                    <Link to="/setting">
+                        <Settings fontSize="large" />
+                    </Link>
+                </Grid>
+                <Grid item>
+                    <Link to="/question">
+                        <Help fontSize="large" />
+                    </Link>
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
